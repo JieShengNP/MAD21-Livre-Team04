a128368e4cb84990bd643ff43a2df341a7ec5188
JieShengNP <s10205049@connect.np.edu.sg>
2021-07-11 03:53:56 +0800

    Add Firebase Implementation

--- app/src/main/java/sg/edu/np/mad/livre/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/livre/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://livre-46ac7-default-rtdb.asia-southeast1.firebasedatabase.app/");
+//        Usage of Firebase Database
+//        DatabaseReference myRef = database.getReference("Key");
+//        myRef.setValue("Value");
     }
 }
