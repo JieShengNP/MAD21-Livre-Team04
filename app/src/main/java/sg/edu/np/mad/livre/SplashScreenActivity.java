0990eee200be678f66a3514188ea1d674fc49380
Chow Yun Cong <S10194206@connect.np.edu.sg>
2021-07-11 00:38:08 +0800

    Add splash screen

---
+++ app/src/main/java/sg/edu/np/mad/livre/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package sg.edu.np.mad.livre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    //Splash screen
+    private static int SPLASH_TIME_OUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                //close this activity
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
