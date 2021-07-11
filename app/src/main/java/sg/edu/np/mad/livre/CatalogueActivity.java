b34f9a74c70d2e253ddc563323cfe362cd4ea91a
Li Min Kua <s10206234@connect.np.edu.sg>
2021-07-11 16:27:46 +0800

    Added search png

---
+++ app/src/main/java/sg/edu/np/mad/livre/CatalogueActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.livre;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CatalogueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalogue);
+    }
+}
