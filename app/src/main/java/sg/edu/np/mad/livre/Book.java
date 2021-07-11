21e7b219dd8c699fc55b720a966759b6fa6062d1
Li Min Kua <s10206234@connect.np.edu.sg>
2021-07-11 15:00:18 +0800

    Added Book object

---
+++ app/src/main/java/sg/edu/np/mad/livre/Book.java
@@ -0,0 +1,70 @@
+package sg.edu.np.mad.livre;
+
+public class Book {
+    public int isbn;
+    public String name;
+    public String blurb;
+    public String thumbnail;
+    public int readSeconds;
+    public boolean isCustom;
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBlurb() {
+        return blurb;
+    }
+
+    public void setBlurb(String blurb) {
+        this.blurb = blurb;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public int getReadSeconds() {
+        return readSeconds;
+    }
+
+    public void setReadSeconds(int readSeconds) {
+        this.readSeconds = readSeconds;
+    }
+
+    public boolean isCustom() {
+        return isCustom;
+    }
+
+    public void setCustom(boolean custom) {
+        isCustom = custom;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn=" + isbn +
+                ", name='" + name + '\'' +
+                ", blurb='" + blurb + '\'' +
+                ", thumbnail='" + thumbnail + '\'' +
+                ", readSeconds=" + readSeconds +
+                ", isCustom=" + isCustom +
+                '}';
+    }
+}
