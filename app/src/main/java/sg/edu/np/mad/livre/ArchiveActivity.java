ecaf53689ec11a46155b1399063306b33b29cea5
JieShengNP <s10205049@connect.np.edu.sg>
2021-07-12 01:05:18 +0800

    Add Access From Library To Archive and Vice-Versa

--- app/src/main/java/sg/edu/np/mad/livre/ArchiveActivity.java
+++ app/src/main/java/sg/edu/np/mad/livre/ArchiveActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
 public class ArchiveActivity extends AppCompatActivity {
 
+    ImageView libraryImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archive);
+
+        libraryImage = findViewById(R.id.archiveArchiveImage);
+        libraryImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         //TODO: Populate ArrayList<ArrayList<Book>> with contents
         ArrayList<ArrayList<Book>> bookNestedList = new ArrayList<>();
--- app/src/main/java/sg/edu/np/mad/livre/LibraryActivity.java
+++ app/src/main/java/sg/edu/np/mad/livre/LibraryActivity.java
@@ -4,16 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
 public class LibraryActivity extends AppCompatActivity {
 
+    ImageView archiveButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+        
+        archiveButton = findViewById(R.id.libraryArchiveImage);
+        archiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LibraryActivity.this, ArchiveActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         //TODO: Populate ArrayList<ArrayList<Book>> with contents
         ArrayList<ArrayList<Book>> bookNestedList = new ArrayList<>();
