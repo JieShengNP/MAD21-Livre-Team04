decd7ebdd1e05c290be8c4e3841848016cb34105
JieShengNP <s10205049@connect.np.edu.sg>
2021-07-12 00:12:39 +0800

    Add RecyclerView to ArchiveActivity

--- app/src/main/java/sg/edu/np/mad/livre/ArchiveActivity.java
+++ app/src/main/java/sg/edu/np/mad/livre/ArchiveActivity.java
@@ -1,8 +1,12 @@
 package sg.edu.np.mad.livre;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class ArchiveActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class ArchiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archive);
+
+        //TODO: Populate ArrayList<ArrayList<Book>> with contents
+        ArrayList<ArrayList<Book>> bookNestedList = new ArrayList<>();
+
+        LibraryAdapter libraryAdapter = new LibraryAdapter(bookNestedList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        RecyclerView recyclerView = findViewById(R.id.archiveRecyclerView);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(libraryAdapter);
     }
 }
