0a45f2b7f5947fda6bc3bfc4ff9626d59f3056fb
Li Min Kua <s10206234@connect.np.edu.sg>
2021-07-15 12:18:56 +0800

    Modified 3 API calls

--- app/src/main/java/sg/edu/np/mad/livre/CatalogueActivity.java
+++ app/src/main/java/sg/edu/np/mad/livre/CatalogueActivity.java
@@ -1,14 +1,12 @@
 package sg.edu.np.mad.livre;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -18,18 +16,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonParser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -38,6 +31,9 @@ public class CatalogueActivity extends AppCompatActivity {
     ArrayList<String> seedList;
     ArrayList<Book> bookList;
 
+    static ArrayList<String> finalSeedList;
+    static ArrayList<Book> finalBookList;
+    static int noOfEntries;
     ImageView libraryImage, temporaryCreateImage;
 
     @Override
@@ -45,21 +41,14 @@ public class CatalogueActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogue);
 
+
         libraryImage = findViewById(R.id.catalogueLibraryTag);
-        libraryImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        libraryImage.setOnClickListener(v -> finish());
 
         temporaryCreateImage = findViewById(R.id.temporaryCreateImage);
-        temporaryCreateImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CatalogueActivity.this, CustomiseBook.class);
-                startActivity(intent);
-            }
+        temporaryCreateImage.setOnClickListener(v -> {
+            Intent intent = new Intent(CatalogueActivity.this, CustomiseBook.class);
+            startActivity(intent);
         });
 
         //Find search icon and set onclicklistener
@@ -67,6 +56,7 @@ public class CatalogueActivity extends AppCompatActivity {
         searchIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                noOfEntries = 0;
                 Log.v("DEBUG", "CLICKED");
                 seedList = new ArrayList<String>();
                 bookList = new ArrayList<Book>();
@@ -82,16 +72,24 @@ public class CatalogueActivity extends AppCompatActivity {
                 //Create JsonObjectRequest object
                 JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                         (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
                             //Handle response
                             @Override
                             public void onResponse(JSONObject response) {
+                                Log.v("DEBUG", "first API CALL");
                                 try {
-
+                                    Log.v("URL", url);
                                     //get jsonarray docs
                                     JSONArray docs = response.getJSONArray("docs");
                                     //append isbn to list
-                                    for (int i = 0; i < 20; i++) {
+                                    int count = 0;
+                                    if (docs.length()>20){
+                                        count = 20;
+                                    }
+                                    else if (docs.length() <= 20) {
+                                        count = docs.length();
+                                    }
+                                    Log.v("count", String.valueOf(count));
+                                    for (int i = 0; i < count; i++) {
                                         JSONObject object = docs.getJSONObject(i);
                                         JSONArray seedArray = object.getJSONArray("seed");
                                         String firstseed = seedArray.get(0).toString();
@@ -118,7 +116,6 @@ public class CatalogueActivity extends AppCompatActivity {
 
                                             continue;
                                         }
-                                        ;
 
 
                                         Book b = new Book();
@@ -129,11 +126,38 @@ public class CatalogueActivity extends AppCompatActivity {
 
 
                                         bookList.add(b);
-
-                                        Log.v("DESC", String.valueOf(bookList.size()));
-                                        Log.v("Book", b.toString());
                                         seedList.add(firstseed);
                                     }
+
+
+noOfEntries = bookList.size();
+
+
+                                    //SECOND API CALL
+                                    Log.v("Debug", "getDescFromAPIOOOOOOOOOOOOOOOO");
+
+
+
+
+                                    ArrayList<String> descList = getDescFromAPI(seedList);
+                                    Log.v("Debug", "getTHUMBSFromAPI");
+                                    ArrayList<String> thumbList = getThumbsfromAPI(bookList);
+                                    Log.v("Debujjg", String.valueOf(thumbList.size()));
+//                                    for (int b = 0; b<bookList.toArray().length; b++){
+//                                        Book book = bookList.get(b);
+//                                        book.blurb = descList.get(b);
+//                                        book.thumbnail = thumbList.get(b);
+//
+//                                        bookList.set(b, book);
+//                                    }
+//
+//
+//                                    RecyclerView rv = findViewById(R.id.catRecyclerView);
+//                                    CatItemsAdapter itemsAdapter = new CatItemsAdapter(bookList);
+//                                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+//                                    rv.setLayoutManager(linearLayoutManager);
+//                                    rv.setAdapter(itemsAdapter);
+
 
                                 } catch (JSONException e) { //catch exception
                                     e.printStackTrace();
@@ -161,122 +185,148 @@ public class CatalogueActivity extends AppCompatActivity {
                 ApiSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
 
 
-
-                ArrayList<String> descList = new ArrayList<>();
-
-                Log.v("DESC121212", String.valueOf(bookList.size()));
-
-                for (int b = 0; b<bookList.size(); b++){
-
-                    Log.v("DESC", "desc");
-                    String requrl ="https://openlibrary.org/" + seedList.get(b) + ".json";
-                    //Create JsonObjectRequest object
-                    JsonObjectRequest reqObj = new JsonObjectRequest
-                            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                                //Handle response
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    try {
-                                        //get jsonarray docs
-
-                                        String desc = response.getString("description");
-                                        descList.add(desc);
-
-                                        Log.v("DESC", desc);
-
-
-                                    } catch (JSONException e) { //catch exception
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }, new Response.ErrorListener() {
-
-                                //handle error response
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-
-                                    new AlertDialog.Builder(getApplicationContext())
-                                            .setTitle("Error!")
-                                            .setMessage("Catalogue is not working at the moment, please try again later")
-                                            .setPositiveButton("OK", null)
-                                            .show();
-                                }
-                            });
-
-                    // Access the RequestQueue through singleton class.
-                    ApiSingleton.getInstance(getApplicationContext()).addToRequestQueue(reqObj);
-
-                }
-
-
-                ArrayList<String> thumbList = new ArrayList<>();
-
-                for (int b = 0; b<bookList.size(); b++){
-                    String requrl ="https://openlibrary.org/api/books?bibkeys=ISBN:"+ bookList.get(b).isbn+"&format=json";
-                    //Create JsonObjectRequest object
-                    JsonObjectRequest reqObj = new JsonObjectRequest
-                            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                                //Handle response
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    String thumb = "";
-                                    try {
-                                        //get jsonarray docs
-
-                                        Iterator<String> keys = response.keys();
-                                        String key;
-                                        if(keys.hasNext()){
-                                            key = keys.next();
-                                            thumb = response.getJSONObject(key).getString("thumbnail_url");
-                                        }
-                                        thumbList.add(thumb);
-
-
-                                    } catch (JSONException e) { //catch exception
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }, new Response.ErrorListener() {
-
-                                //handle error response
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-
-                                    new AlertDialog.Builder(getApplicationContext())
-                                            .setTitle("Error!")
-                                            .setMessage("Catalogue is not working at the moment, please try again later")
-                                            .setPositiveButton("OK", null)
-                                            .show();
-                                }
-                            });
-
-                    // Access the RequestQueue through singleton class.
-                    ApiSingleton.getInstance(getApplicationContext()).addToRequestQueue(reqObj);
-
-                }
-
-
-
-                for (int b = 0; b<bookList.toArray().length; b++){
-                    Book book = bookList.get(b);
-                    book.blurb = descList.get(b);
-                    book.thumbnail = thumbList.get(b);
-
-                    bookList.set(b, book);
-                }
-
-
-                RecyclerView rv = findViewById(R.id.catRecyclerView);
-                CatItemsAdapter itemsAdapter = new CatItemsAdapter(bookList);
-                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-                rv.setLayoutManager(linearLayoutManager);
-                rv.setAdapter(itemsAdapter);
-
-
-
             }
         });
     }
+
+    public ArrayList<String> getThumbsfromAPI(ArrayList<Book> bookList) {
+        Log.v("DeAAAAAAAAb", "getTHUMBSFromAPI");
+        ArrayList<String> thumbList = new ArrayList<>();
+
+        for (int b = 0; b < bookList.size(); b++)  {
+            String requrl = "https://openlibrary.org/api/books?bibkeys=ISBN:" + bookList.get(b).isbn + "&format=json";
+            Log.v("THE URL", requrl);
+            //Create JsonObjectRequest object
+            //handle error response
+            JsonObjectRequest reqObj = new JsonObjectRequest
+                    (Request.Method.GET, requrl, null, new Response.Listener<JSONObject>() {
+
+                        //Handle response
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            String thumb = "";
+                            try {
+                                //get jsonarray docs
+
+                                Iterator<String> keys = response.keys();
+                                String key;
+                                if (keys.hasNext()) {
+                                    key = keys.next();
+                                    if (response.getJSONObject(key).has("thumbnail_url")) {
+                                        thumb = response.getJSONObject(key).getString("thumbnail_url");
+                                        thumb = thumb.substring(0, thumb.length() - 5) + "L.jpg";
+                                    }
+                                }
+                                Log.v("thumby", thumb + " ememememem");
+                                thumbList.add(thumb);
+
+
+                            } catch (JSONException e) { //catch exception
+                                thumbList.add(thumb);
+                                e.printStackTrace();
+                            }
+                            if(thumbList.size() == noOfEntries){
+
+                                updateList(bookList, thumbList, null);
+                            }
+
+                        }
+
+                    }, error -> new AlertDialog.Builder(getApplicationContext())
+                            .setTitle("Error!")
+                            .setMessage("Catalogue is not working at the moment, please try again later")
+                            .setPositiveButton("OK", null)
+                            .show());
+
+            // Access the RequestQueue through singleton class.
+            ApiSingleton.getInstance(getApplicationContext()).addToRequestQueue(reqObj);
+
+        }
+        return thumbList;
+    }
+
+
+
+    public ArrayList<String> getDescFromAPI(List<String> seedList) {
+        ArrayList<String> descList = new ArrayList<>();
+        for (int b = 0; b < seedList.size(); b++) {
+
+            String requrl = "https://openlibrary.org/" + seedList.get(b) + ".json";
+            //Create JsonObjectRequest object
+            JsonObjectRequest reqObj = new JsonObjectRequest
+                    (Request.Method.GET, requrl, null, new Response.Listener<JSONObject>() {
+
+                        //Handle response
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                            try {
+                                //get jsonarray docs
+
+                                String desc = "Unavailable";
+                                if (response.has("description")) {
+                                    if (response.get("description") instanceof String) {
+                                        desc = response.getString("description");
+                                    } else if (response.getJSONObject("description").has("value")) {
+                                        desc = response.getJSONObject("description").getString("value");
+
+                                    }
+                                } else if (response.has("subjects")) {
+                                    JSONArray array = response.getJSONArray("subjects");
+                                    if (array.length() != 0) {
+                                        desc = "";
+                                        for (int j = 0; j < array.length(); j++) {
+                                            desc += array.getString(j) + ", ";
+                                        }
+                                        desc = desc.substring(0, desc.length() - 2) + ".";
+                                    }
+                                } else if (response.has("subtitle")) {
+                                    desc = response.getString("subtitle");
+                                }
+                                Log.v("HELLOOO", desc);
+                                descList.add(desc);
+
+
+                            } catch (JSONException e) { //catch exception
+                                Log.v("aaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaa");
+                                e.printStackTrace();
+                                descList.add("Unavailable");
+
+                            }
+
+
+                        }
+                    }, new Response.ErrorListener() {
+
+                        //handle error response
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            new AlertDialog.Builder(getApplicationContext())
+                                    .setTitle("Error!")
+                                    .setMessage("Catalogue is not working at the moment, please try again later")
+                                    .setPositiveButton("OK", null)
+                                    .show();
+                        }
+                    });
+
+            // Access the RequestQueue through singleton class.
+            ApiSingleton.getInstance(getApplicationContext()).addToRequestQueue(reqObj);
+
+        }
+
+        return descList;
+
+    }
+
+
+    public void updateList(ArrayList<Book> bookList, ArrayList<String> thumbList, ArrayList<String> descList){
+
+    }
+
+
+
+
+
+
+
 }
