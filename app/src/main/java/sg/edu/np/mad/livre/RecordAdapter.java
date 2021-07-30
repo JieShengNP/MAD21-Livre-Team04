f1f388ecf7d2e1f2eefa480ab74c2a472d53da67
Chow Yun Cong <S10194206@connect.np.edu.sg>
2021-07-30 17:54:05 +0800

    Modify to handle custom images

--- app/src/main/java/sg/edu/np/mad/livre/RecordAdapter.java
+++ app/src/main/java/sg/edu/np/mad/livre/RecordAdapter.java
@@ -1,6 +1,9 @@
 package sg.edu.np.mad.livre;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.graphics.drawable.Drawable;
 
@@ -34,14 +37,46 @@ public class RecordAdapter extends RecyclerView.Adapter<RecordViewHolder> {
     public void onBindViewHolder(RecordViewHolder holder, int position) {
         Records record = recordsArrayList.get(position);
         DBHandler dbHandler = new DBHandler(holder.bookCover.getContext());
-        String URL = dbHandler.FindBookByISBN(record.getIsbn()).getThumbnail();
-        if (URL != null){
-            Picasso.get()
-                    .load(URL)
-                    .placeholder(holder.bookCover.getContext().getResources().getDrawable(R.drawable.shelf_bust))
-                    .resize(90, 140)
-                    .into(holder.bookCover);
+        Book book = dbHandler.FindBookByISBN(record.getIsbn());
+//        if (URL != null){
+//            Picasso.get()
+//                    .load(URL)
+//                    .placeholder(holder.bookCover.getContext().getResources().getDrawable(R.drawable.shelf_bust))
+//                    .resize(90, 140)
+//                    .into(holder.bookCover);
+//        }
+
+        if ((book.getThumbnail() != null))
+        {
+            if (book.isCustom())
+            {
+                if(book.getThumbnail().equals("Unavailable"))
+                {
+                    holder.bookCover.setImageDrawable(holder.bookCover.getContext()
+                            .getResources().getDrawable(R.drawable.shelf_bust));
+                }
+                else
+                {
+                    byte[] decodedString = Base64.decode(book.getThumbnail(), Base64.DEFAULT);
+                    Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                    holder.bookCover.setImageBitmap(decodedByte);
+                }
+            }
+            else
+            {
+                Picasso.get()
+                        .load(book.getThumbnail())
+                        .placeholder(holder.bookCover.getContext().getResources().getDrawable(R.drawable.shelf_bust))
+                        .resize(90, 140)
+                        .into(holder.bookCover);
+            }
         }
+        else
+        {
+            holder.bookCover.setImageDrawable(holder.bookCover.getContext()
+                    .getResources().getDrawable(R.drawable.shelf_bust));
+        }
+
         int readTime = record.getTimeReadSec();
         String Time = "   Read Time: ";
         //less than 1 minute
@@ -72,5 +107,9 @@ public class RecordAdapter extends RecyclerView.Adapter<RecordViewHolder> {
         return recordsArrayList.size();
     }
 
+    public void SetImage()
+    {
+
+    }
 
 }
