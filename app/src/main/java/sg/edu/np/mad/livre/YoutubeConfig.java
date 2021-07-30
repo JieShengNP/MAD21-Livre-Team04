392d6e68bdefcea0590dcec231257822803e1786
Chow Yun Cong <S10194206@connect.np.edu.sg>
2021-07-30 12:36:52 +0800

    Add YoutubeConfig

---
+++ app/src/main/java/sg/edu/np/mad/livre/YoutubeConfig.java
@@ -0,0 +1,12 @@
+package sg.edu.np.mad.livre;
+
+public class YoutubeConfig {
+    public YoutubeConfig(){}
+
+    private static final String API_KEY = "AIzaSyCnW342fMgc5t2DsWF9iQOZn47Iu-w0Wh4";
+
+    public static String getApiKey()
+    {
+        return  API_KEY;
+    }
+}
