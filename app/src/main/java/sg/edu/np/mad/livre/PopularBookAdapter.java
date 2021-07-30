fdedc4e195ba149087f4fe15bc0e1b52e95b1f00
JieShengNP <s10205049@connect.np.edu.sg>
2021-07-31 07:29:08 +0800

    Create PopularBook RecyclerView

---
+++ app/src/main/java/sg/edu/np/mad/livre/PopularBookAdapter.java
@@ -0,0 +1,72 @@
+package sg.edu.np.mad.livre;
+
+import android.graphics.drawable.Drawable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.RelativeSizeSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class PopularBookAdapter extends RecyclerView.Adapter<PopularBookViewHolder> {
+
+    ArrayList<PopularBook> bookList;
+
+    public PopularBookAdapter(ArrayList<PopularBook> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public PopularBookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_popularbooks, parent, false);
+        return new PopularBookViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(PopularBookViewHolder holder, int position) {
+        Drawable defaultImage = holder.bookThumbnail.getContext().getResources().getDrawable(R.drawable.shelf_bust);
+        holder.bookTitle.setText(bookList.get(position).title);
+        Picasso.get()
+                .load(bookList.get(position).thumbnail)
+                .placeholder(defaultImage)
+                .resize(90, 140)
+                .into(holder.bookThumbnail);
+        holder.bookAuthorYear.setText(bookList.get(position).author + " . " + bookList.get(position).year);
+        holder.bookDesc.setText(bookList.get(position).blurb);
+        String timeFormat = "";
+        float calculatedTime = bookList.get(position).totalTime;
+
+        // Format in 2dp
+        DecimalFormat df = new DecimalFormat("#0.00");
+        String timeSpent = "";
+        if (bookList.get(position).totalTime < 60) {
+            timeFormat = " Total Seconds Read";
+            timeSpent = String.valueOf(bookList.get(position).totalTime);
+        } else if ((calculatedTime = (bookList.get(position).totalTime / 60)) < 60) {
+            timeFormat = " Total Minutes Read";
+            timeSpent = df.format(calculatedTime);
+        } else if ((calculatedTime = (bookList.get(position).totalTime / 3600)) < 60) {
+            timeFormat = " Total Hours Read";
+            timeSpent = df.format(calculatedTime);
+        }
+        // Make the number text bigger in TextView
+        SpannableStringBuilder readerSpannable = new SpannableStringBuilder(bookList.get(position).totalReaders + " Total Readers");
+        readerSpannable.setSpan(new RelativeSizeSpan(1.5f), 0, String.valueOf(bookList.get(position).totalReaders).length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SpannableStringBuilder timeSpannable = new SpannableStringBuilder(timeSpent + timeFormat);
+        timeSpannable.setSpan(new RelativeSizeSpan(1.5f), 0, timeSpent.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        holder.bookStats.setText(TextUtils.concat(readerSpannable, "\n", timeSpannable));
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookList.size();
+    }
+}
---
+++ app/src/main/java/sg/edu/np/mad/livre/PopularBookViewHolder.java
@@ -0,0 +1,22 @@
+package sg.edu.np.mad.livre;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PopularBookViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView bookThumbnail;
+    TextView bookTitle, bookAuthorYear, bookDesc, bookStats;
+
+    public PopularBookViewHolder(View itemView) {
+        super(itemView);
+        bookThumbnail = itemView.findViewById(R.id.popularBookThumbnail);
+        bookTitle = itemView.findViewById(R.id.popularBookTitle);
+        bookAuthorYear = itemView.findViewById(R.id.popularBookAuthorYear);
+        bookDesc = itemView.findViewById(R.id.popularBookDesc);
+        bookStats = itemView.findViewById(R.id.popularBookStats);
+    }
+}
