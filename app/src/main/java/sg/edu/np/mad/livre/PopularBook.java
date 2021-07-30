81ae712109dd6e4fb0264f0299635f81447f43dd
JieShengNP <s10205049@connect.np.edu.sg>
2021-07-31 07:27:19 +0800

    Create PopularBook Class

---
+++ app/src/main/java/sg/edu/np/mad/livre/PopularBook.java
@@ -0,0 +1,12 @@
+package sg.edu.np.mad.livre;
+
+public class PopularBook {
+    public String isbn;
+    public String title;
+    public String author;
+    public String blurb;
+    public String year;
+    public String thumbnail;
+    public int totalReaders;
+    public long totalTime;
+}
