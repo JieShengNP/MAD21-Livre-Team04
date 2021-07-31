1af7a6898b32b591ef7cb0ab2b96fd826db9af7a
JieShengNP <s10205049@connect.np.edu.sg>
2021-07-31 16:18:10 +0800

    Fix re-creation of firebase on login

--- app/src/main/java/sg/edu/np/mad/livre/SignInActivity.java
+++ app/src/main/java/sg/edu/np/mad/livre/SignInActivity.java
@@ -244,9 +244,8 @@ public class SignInActivity extends AppCompatActivity {
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         startActivity(intent);
                     } else {
-                        Toast.makeText(SignInActivity.this, "An error has occurred.", Toast.LENGTH_SHORT).show();
+                        CreateDataInFirebase(userID, userEmail);
                     }
-                    CreateDataInFirebase(userID, userEmail);
                 }
             }
         });
