d50950ebd69db61f11aefa4c7b20696c13b45689
JieShengNP <s10205049@connect.np.edu.sg>
2021-08-01 08:44:58 +0800

    Update Test02_LibraryActivityTest.java

--- app/src/androidTest/java/sg/edu/np/mad/livre/Test02_LibraryActivityTest.java
+++ app/src/androidTest/java/sg/edu/np/mad/livre/Test02_LibraryActivityTest.java
@@ -6,18 +6,22 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeDown;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Test02_LibraryActivityTest {
 
     @Rule
@@ -29,27 +33,38 @@ public class Test02_LibraryActivityTest {
     }
 
     @Test
-    public void testRecyclerViewVisibility() {
+    public void test1_RecyclerViewVisibility() {
         onView(withId(R.id.libraryRecyclerView)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void testRecyclerListContent() {
+    public void test2_RecyclerListContent() {
         onView(withId(R.id.libraryRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (Exception e) {
         }
         intended(hasComponent(BookDetails.class.getName()));
     }
 
     @Test
-    public void testBookArchiveToggle() {
+    public void test3_BookArchiveToggle() {
         onView(withId(R.id.libraryRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        swipeUp();
         onView(withId(R.id.detToggleArcBtn)).check(matches(isDisplayed())).check(matches(withText("Move to Archive")));
         onView(withId(R.id.detToggleArcBtn)).perform(click());
         onView(withId(R.id.detToggleArcBtn)).check(matches(isDisplayed())).check(matches(withText("Move to Library")));
         onView(withId(R.id.detToggleArcBtn)).perform(click());
+    }
+
+    @Test
+    public void test4_OpenArchive(){
+        onView(withId(R.id.libraryArchiveImage)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+        }
+        intended(hasComponent(ArchiveActivity.class.getName()));
     }
 
     @After
